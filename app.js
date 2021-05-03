ccf79d076d7da964914da1b567698accd434dc92
syatom <53854902+syatom@users.noreply.github.com>
2021-05-03 21:31:10 +0100

    new functions added

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const canvas = document.querySelector('#myCanvas')
 const ctx = canvas.getContext("2d")
 
 
-let x = canvas.width/2;
-let y = canvas.height-30;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 const ballRadius = 10
 let paddleHeight = 10
 let paddleWidth = 120
-let paddleX = (canvas.width-paddleWidth) / 2
+let paddleX = (canvas.width - paddleWidth) / 2
 let rightPressed = false
 let leftPressed = false
+
+// keeping track of the score
+let score = 0
+
+// number of lives
+let lives = 3
 
 // Setting up the brick variables
 let brickRowCount = 3
@@ -24,10 +30,10 @@ let brickOffsetTop = 30
 let brickOffsetLeft = 30
 
 const bricks = [];
-for(let c = 0; c < brickColumnCount; c++) {
+for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
-    for(let r=0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
@@ -35,7 +41,7 @@ console.log(bricks)
 
 const drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -43,62 +49,78 @@ const drawBall = () => {
 
 const drawPaddle = () => {
     ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
 const drawBricks = () => {
-    for(let c = 0; c < brickColumnCount; c++) {
-        for(let r = 0; r < brickRowCount; r++) {
-            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
 
-const draw = ()  => {
-    ctx.clearRect(0,0, canvas.width, canvas.height)
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawBall()
     drawPaddle()
     drawBricks()
+    collisionDetection()
+    drawScore()
+    drawLives()
     x += dx
     y += dy
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    
-    if(y + dy < ballRadius) {
+
+    if (y + dy < ballRadius) {
         dy = -dy;
-    } else if ( y + dy > canvas.height-ballRadius) {
+    } else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy
             // dx ++
             // dy --
         } else {
-            alert("GAME OVER!")
-            document.location.reload()
-            clearInterval(interval)
+            lives--
+            if (!lives) {
+                alert("GAME OVER!")
+                document.location.reload()
+                clearInterval(interval)
+            } else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+
+
         }
-        
+
     }
 
-    if(rightPressed) {
+    if (rightPressed) {
         paddleX += 7
-        if ( paddleX + paddleWidth > canvas.width ) {
+        if (paddleX + paddleWidth > canvas.width) {
             paddleX = canvas.width - paddleWidth
         }
-    } else if(leftPressed) {
+    } else if (leftPressed) {
         paddleX -= 7
-        if (paddleX < 0 ) {
+        if (paddleX < 0) {
             paddleX = 0
         }
     }
@@ -124,8 +146,48 @@ const keyUpHandler = (e) => {
     }
 }
 
+const mouseMoveHandler = (e) => {
+    let relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
+    }
+}
+
+const collisionDetection = () => {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            let b = bricks[c][r];
+            if (b.status === 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0
+                    score++
+                    if (score === brickRowCount * brickColumnCount) {
+                        alert(`YOU WIN, CONGRATULATIONS! score: ${score}`);
+                        document.location.reload();
+                        clearInterval(interval); // Needed for Chrome to end game
+                    }
+                }
+            }
+        }
+    }
+}
+
+const drawScore = () => {
+    ctx.font = "16px Arial"
+    ctx.fillStyle = "#0095DD"
+    ctx.fillText(`Score: ${score}`, 8, 20)
+}
+
+const drawLives = () => {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
+
 document.addEventListener('keydown', keyDownHandler, false)
 document.addEventListener('keyup', keyUpHandler, false)
+document.addEventListener("mousemove", mouseMoveHandler, false)
 
 let interval = setInterval(draw, 10)
 
